62c99b6e59d08ad47c5ba6c379026222587d0ad1
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-05-20 20:20:04 -0600

    WIP - added tile_value_array_maker method and successfully tested length and class of values

--- lib/board.rb
+++ lib/board.rb
@@ -2,4 +2,17 @@ require 'pry'
 require './lib/tile'
 
 class Board
+
+  def tile_value_array_maker
+    tile_names = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    tile_value_array = []
+
+    tile_names.map do |each|
+      each = Tile.new.tile_valuation
+      tile_value_array << each
+    end
+
+  p tile_value_array
+  end
+
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -12,4 +12,14 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, board
   end
 
+  def test_board_creates_array_with_16_tile_values
+    board = Board.new()
+    assert_equal 16, board.tile_value_array_maker.count
+  end
+
+  def test_value_array_maker_elements_are_strings
+    board = Board.new()
+    assert_equal String, board.tile_value_array_maker[0].class
+    assert_equal String, board.tile_value_array_maker[15].class
+  end
 end
